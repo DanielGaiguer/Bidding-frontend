4a5edd11c3856652855de63d9c6bedc226c8e5ad
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-05-20 14:05:38 -0300

    initial commit

---
+++ src/test/java/com/main/bidding_frontend/BiddingFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.bidding_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
